c255f0e3687d2b24ef91fe66815a9837ea9f7ea0
tran.dinh.manh@fujitechjsc.com <tran.dinh.manh@fujitechjsc.com>
2021-11-17 00:03:14 +0700

    create project

---
+++ app/src/main/java/com/fitness/feature/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.fitness.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.fitness.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash)
+    }
+}
